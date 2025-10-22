f4c9bd3470a15f1cfd7ef4a88849529c7d3d7616
sebastian.vegar2015 <sebastian.vegar2015@gmail.com>
2025-10-22 03:40:29 +0000

    activo en header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,30 +1,23 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-  // Estado para controlar la visibilidad del menú en dispositivos móviles
-  isMenuOpen: boolean = false;
+export class HeaderComponent {
+  isMenuOpen = false;
 
   constructor(private router: Router) {}
 
-  ngOnInit(): void {
-    // Si necesitas inicializar algo, hazlo aquí.
-  }
-
-  // Lógica para alternar el menú de hamburguesa
   toggleMenu(): void {
     this.isMenuOpen = !this.isMenuOpen;
   }
 
-  // Lógica para navegar y cerrar el menú
-  navigateTo(route: string): void {
-    this.router.navigate([route]);
-    this.isMenuOpen = false; // Cerrar menú después de navegar (útil en móvil)
+  closeMenu(): void {
+    this.isMenuOpen = false;
   }
 }
